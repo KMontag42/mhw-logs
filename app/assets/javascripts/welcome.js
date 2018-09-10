aba0fe658077b9320ce9a030055a8c9f5278a6ec
Kyle Montag <kmontag@salsify.com>
2018-09-10 14:13:04 -0400

    More dank updates

---
+++ app/assets/javascripts/welcome.js
@@ -0,0 +1,5 @@
+document.addEventListener("turbolinks:load", x => {
+  if (document.body.contains(document.querySelector(".welcome.index"))) {
+    // this is where page specific JS goes
+  }
+});
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  def active_link_to(name = nil, route = nil, html_options = nil, &block)
+    active_class = html_options[:active] || "is-active"
+    html_options.delete(:active)
+    html_options[:class] = "#{html_options[:class]} #{active_class}" if current_page?(route)
+    link_to(name, route, html_options, &block)
+  end
 end
